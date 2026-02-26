13d3d746b80467321a72a701e2cd5ede880b62f3
Akpali Francis Ezennaya <francisakpali9@gmail.com>
2026-02-26 19:54:37 +0100

    Update app-header.tsx

--- src/app/components/app-header.tsx
+++ src/app/components/app-header.tsx
@@ -118,9 +118,8 @@ export function AppHeader({
                       
                         <Settings className="w-3.5 h-3.5 text-[#389C9A]" /> {userType === 'business' ? 'Business Settings' : 'Settings'}
                       </button>
-                      <Link  to="/login/portal" 
-               >
-               
+                      <Link  to="/ " 
+                       >   
                       <button
                         className="w-full text-left px-4 py-3 text-[10px] font-black uppercase tracking-widest hover:bg-[#1D1D1D] hover:text-white text-red-500 flex items-center gap-3 transition-colors italic"
                       >
