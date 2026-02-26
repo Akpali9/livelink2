feeeed22c355718e9ce47a336416f5860789b1e4
Akpali Francis Ezennaya <francisakpali9@gmail.com>
2026-02-26 18:43:10 +0100

    Update home.tsx

--- src/app/screens/home.tsx
+++ src/app/screens/home.tsx
@@ -49,20 +49,14 @@ export function Home() {
             Sign In
             <ArrowRight className="w-5 h-5 text-[#389C9A]" />
           </Link>
-          <Link 
-            to="/become-creator" 
+           <Link 
+            to="/login/portal"  
             className="group flex items-center justify-between bg-[#1D1D1D] text-white p-6 font-black uppercase tracking-tight transition-all active:scale-[0.98] italic"
           >
             Become a Creator
             <ArrowRight className="w-5 h-5 transition-transform group-hover:translate-x-1 text-[#389C9A]" />
           </Link>
-          <Link 
-            to="/business/dashboard" 
-            className="flex items-center justify-between border-2 border-[#1D1D1D] text-[#1D1D1D] p-6 font-black uppercase tracking-tight transition-all active:scale-[0.98] hover:bg-[#1D1D1D] hover:text-white italic"
-          >
-            Business Dashboard
-            <ArrowRight className="w-5 h-5 text-[#389C9A]" />
-          </Link>
+          
           <Link 
             to="/become-business" 
             className="flex items-center justify-between border-2 border-[#1D1D1D] text-[#1D1D1D] p-6 font-black uppercase tracking-tight transition-all active:scale-[0.98] hover:bg-[#1D1D1D] hover:text-white italic"
@@ -77,8 +71,8 @@ export function Home() {
             Find Brands
             <ArrowRight className="w-5 h-5 text-[#389C9A]" />
           </Link>
-          <Link 
-            to="/browse" 
+           <Link 
+            to="/login/portal"  
             className="flex items-center justify-between border-2 border-[#1D1D1D] text-[#1D1D1D] p-6 font-black uppercase tracking-tight transition-all active:scale-[0.98] hover:bg-[#1D1D1D] hover:text-white italic"
           >
             Find Creators
