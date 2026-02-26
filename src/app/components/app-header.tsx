dfa44386e574868b55fe08398345a86183f3cd65
Akpali Francis Ezennaya <francisakpali9@gmail.com>
2026-02-26 19:34:39 +0100

    Update app-header.tsx

--- src/app/components/app-header.tsx
+++ src/app/components/app-header.tsx
@@ -115,14 +115,14 @@ export function AppHeader({
                           setShowProfileMenu(false);
                         }} 
                         className="w-full text-left px-4 py-3 text-[10px] font-black uppercase tracking-widest hover:bg-[#1D1D1D] hover:text-white flex items-center gap-3 transition-colors italic"
-                      >
+                      
                         <Settings className="w-3.5 h-3.5 text-[#389C9A]" /> {userType === 'business' ? 'Business Settings' : 'Settings'}
                       </button>
                       <Link  to="/login/portal" 
                >
                
                       <button 
-                          onClick={() => backPath ? navigate(backPath) : navigate(0)}
+                          onClick={() => backPath ? navigate(backPath) : navigate(1)}
                         className="w-full text-left px-4 py-3 text-[10px] font-black uppercase tracking-widest hover:bg-[#1D1D1D] hover:text-white text-red-500 flex items-center gap-3 transition-colors italic"
                       >
                         <LogOut className="w-3.5 h-3.5" /> Logout
